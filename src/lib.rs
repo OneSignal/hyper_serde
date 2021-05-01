2ad89560a9360536cb0bc11872a32c5b0125b68f
Nate Mara <nate@onesignal.com>
2021-04-30 17:23:28 -0700

    Update to support hyper 0.14

--- src/lib.rs
+++ src/lib.rs
@@ -375,7 +375,7 @@ impl<'de> Deserialize<'de> for De<Headers> {
                 let capacity = cmp::min(visitor.size_hint().unwrap_or(0), 64);
                 let mut values = Vec::with_capacity(capacity);
                 while let Some(v) = visitor.next_element::<ByteBuf>()? {
-                    values.push(v.into());
+                    values.push(v.into_vec());
                 }
                 Ok(Value(values))
             }
